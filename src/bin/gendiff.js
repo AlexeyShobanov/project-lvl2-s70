af127aaff9ce4f3f0319a8a0289f0a5f88c1056f
Alexey Shobanov <shobanov@gmail.com>
2017-03-20 23:56:18 +0300

    The first state of the second level

---
+++ src/bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+
+commander
+.version('0.0.2')
+.description('Compares two configuration files and shows a difference')
+.arguments('<first_config> <second_config>')
+.option('-f, --format [type]', 'Output format')
+.parse(process.argv);
