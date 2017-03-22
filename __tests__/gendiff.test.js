ca1c1751ddacc7638cbf25d7017060ad12777421
Alexey Shobanov <shobanov@gmail.com>
2017-03-22 19:23:53 +0300

    The second state of the second level v0.0.9

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,12 @@
 import assert from 'assert';
 import { readConfigFile, parseJson, compareObj } from '../src/commonFile';
 
-const originStrData1 = readConfigFile('./before.json');
-const originStrData2 = readConfigFile('./after.json');
+const originStrData1 = readConfigFile('./__tests__/before.json');
+const originStrData2 = readConfigFile('./__tests__/after.json');
 const objForCompare1 = parseJson(originStrData1);
 const objForCompare2 = parseJson(originStrData2);
 const resultCompare = compareObj(objForCompare1, objForCompare2);
-const originStrBench = readConfigFile('./result.json');
+const originStrBench = readConfigFile('./__tests__/result.json');
 const objBench = parseJson(originStrBench);
 
 test(`The difference between\n${originStrData1}\nand\n${originStrData2}\nequal\n${originStrBench}`, () => {
