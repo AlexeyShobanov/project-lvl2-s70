cfd2f08dc3abfaeb0c454e82ed612c3595b6646c
Alexey Shobanov <shobanov@gmail.com>
2017-03-22 17:37:18 +0300

    The second state of the second level v0.0.6

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,8 +7,15 @@ const optionsAndArgs = parseCommandLine();
 const pathFirstConfig = optionsAndArgs[0][0];
 const pathSecondConfig = optionsAndArgs[0][1];
   // const format = optionsAndArgs[1];
-const originStrData1 = readConfigFile(pathFirstConfig);
-const originStrData2 = readConfigFile(pathSecondConfig);
-const objForCompare1 = parseJson(originStrData1);
-const objForCompare2 = parseJson(originStrData2);
-const resultCompare = compareObj(objForCompare1, objForCompare2);
+const gendiff = (path1, path2) => {
+  const originStrData1 = readConfigFile(path1);
+  const originStrData2 = readConfigFile(path2);
+  const objForCompare1 = parseJson(originStrData1);
+  const objForCompare2 = parseJson(originStrData2);
+  const resultCompare = compareObj(objForCompare1, objForCompare2);
+  return resultCompare;
+};
+
+gendiff(pathFirstConfig, pathSecondConfig);
+
+export default gendiff;
