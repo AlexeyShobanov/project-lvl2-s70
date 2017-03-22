898632ea11ce3e96b571ef3dee421c1a6413faaa
Alexey Shobanov <shobanov@gmail.com>
2017-03-22 19:00:25 +0300

    The second state of the second level v0.0.8

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,20 @@
 import assert from 'assert';
 import { readConfigFile, parseJson, compareObj } from '../src/commonFile';
 
-const originStrData1 = readConfigFile('/Users/alexey/projects/project-lvl2-s70/__tests__/before.json');
-const originStrData2 = readConfigFile('/Users/alexey/projects/project-lvl2-s70/__tests__/after.json');
+const originStrData1 = readConfigFile('./before.json');
+const originStrData2 = readConfigFile('./after.json');
 const objForCompare1 = parseJson(originStrData1);
 const objForCompare2 = parseJson(originStrData2);
 const resultCompare = compareObj(objForCompare1, objForCompare2);
-const originStrBench = readConfigFile('/Users/alexey/projects/project-lvl2-s70/__tests__/result.json');
+const originStrBench = readConfigFile('./result.json');
 const objBench = parseJson(originStrBench);
 
 test(`The difference between\n${originStrData1}\nand\n${originStrData2}\nequal\n${originStrBench}`, () => {
   assert.deepEqual(resultCompare, objBench);
 });
+
+/* const originStrBench = readConfigFile('./result.json');
+const objBench = parseJson(originStrBench);
+const resultCompare = gendiff('before.json', 'after.json');
+const originStrData1 = readConfigFile('./before.json');
+const originStrData2 = readConfigFile('./after.json'); */
