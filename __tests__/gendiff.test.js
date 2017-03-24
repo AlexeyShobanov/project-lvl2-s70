e08c40eb3c97d14fec1863949f19de0e9992706d
Alexey Shobanov <shobanov@gmail.com>
2017-03-24 10:29:45 +0300

    step 2 - changes in gendiff, test and makefile

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,4 @@
-import assert from 'assert';
-// import _ from 'lodash';
-import gendiff from '../src/';
-
-/* const result = _.fromPairs([
-  [' host', 'hexlet.io'],
-  ['+ timeout', 20],
-  ['- timeout', 50],
-  ['- proxy', '123.234.53.22'],
-  ['+ verbose', true],
-]); */
+import { gendiffAsObj } from '../src/';
 
 const result = {};
 result[' host'] = 'hexlet.io';
@@ -20,17 +10,17 @@ result['+ verbose'] = true;
 test('The difference between a couple of files json', () => {
   const pathToBeforJson = './__tests__/__fixtures__/before.json';
   const pathToAfterJson = './__tests__/__fixtures__/after.json';
-  assert.deepEqual(gendiff(pathToBeforJson, pathToAfterJson), result);
+  expect(gendiffAsObj(pathToBeforJson, pathToAfterJson)).toEqual(result);
 });
 
 test('The difference between a couple of files yaml', () => {
   const pathToBeforYaml = './__tests__/__fixtures__/before.yml';
   const pathToAfterYaml = './__tests__/__fixtures__/after.yml';
-  assert.deepEqual(gendiff(pathToBeforYaml, pathToAfterYaml), result);
+  expect(gendiffAsObj(pathToBeforYaml, pathToAfterYaml)).toEqual(result);
 });
 
-test('The difference between a couple of files ini', () => {
+/* test('The difference between a couple of files ini', () => {
   const pathToBeforIni = './__tests__/__fixtures__/before.ini';
   const pathToAfterIni = './__tests__/__fixtures__/after.ini';
-  assert.deepEqual(gendiff(pathToBeforIni, pathToAfterIni), result);
-});
+  expect(gendiff(pathToBeforIni, pathToAfterIni)).toEqual(result);
+}); */
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import fs from 'fs';
 import JsonData from './JsonData';
 import YamlData from './YamlData';
 import IniData from './IniData';
-// import readConfigFile from './commonFile';
 
 const compareObj = (inputObj1, inputObj2) => {
   const sumKeys = _.union(_.keys(inputObj1), _.keys(inputObj2));
@@ -40,10 +39,21 @@ const createDataClass = (pathToFile) => {
   return 1;
 };
 
-export default (pathToFile1, pathToFile2) => {
+export const gendiffAsObj = (pathToFile1, pathToFile2) => {
   const data1 = createDataClass(pathToFile1);
   const data2 = createDataClass(pathToFile2);
   const objForCompare1 = data1.parse();
   const objForCompare2 = data2.parse();
   return compareObj(objForCompare1, objForCompare2);
+};
+
+export default (pathToFile1, pathToFile2) => {
+  /* const data1 = createDataClass(pathToFile1);
+  const data2 = createDataClass(pathToFile2);
+  const objForCompare1 = data1.parse();
+  const objForCompare2 = data2.parse();
+  const result = compareObj(objForCompare1, objForCompare2); */
+  const result = gendiffAsObj(pathToFile1, pathToFile2);
+  console.log(JSON.stringify(result, null, '\t'));
+  return 0;
 };
