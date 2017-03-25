efbfd50fa65387cf17efcbd72aa3a0d0d817ae0c
Alexey Shobanov <shobanov@gmail.com>
2017-03-26 01:54:57 +0300

    step 5 - Not completed

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 		npm install
 
 start:
-		npm run babel-node -- 'src/bin/gendiff.js' ./__tests__/__fixtures__/before.json ./__tests__/__fixtures__/after.json
+		npm run babel-node -- 'src/bin/gendiff.js' ./__tests__/__fixtures__/_before.ini ./__tests__/__fixtures__/_after.ini
 
 publish:
 		npm publish
--- src/index.js
+++ src/index.js
@@ -3,29 +3,45 @@ import _ from 'lodash';
 import fs from 'fs';
 import getParser from './parsers';
 
-const compareStr = (inputObj1, inputObj2) => {
+const makeAstForCompare = (inputObj1, inputObj2) => {
   const sumKeys = _.union(_.keys(inputObj1), _.keys(inputObj2));
-  const result = _.reduce(sumKeys, (acc, key) => {
+  const ast = _.reduce(sumKeys, (acc, key) => {
     if (_.has(inputObj1, key) && _.has(inputObj2, key)) {
-      if (_.isEqual(inputObj1[key], inputObj2[key])) {
-        return acc.concat(`  ${key}: ${inputObj1[key]}`);
+      if (inputObj1[key] instanceof Object) {
+        return [...acc, { stat: ' ', key, value: '', child: makeAstForCompare(inputObj1[key], inputObj2[key]) }];
+      } else if (_.isEqual(inputObj1[key], inputObj2[key])) {
+        return [...acc, { stat: ' ', key, value: inputObj1[key], child: [] }];
       }
-      return acc.concat(`+ ${key}: ${inputObj2[key]}`, `- ${key}: ${inputObj1[key]}`);
+      return [...acc, { stat: '+', key, value: inputObj2[key], child: [] }, { stat: '-', key, value: inputObj1[key], child: [] }];
     } else if (_.has(inputObj1, key)) {
-      return acc.concat(`- ${key}: ${inputObj1[key]}`);
+      if (inputObj1[key] instanceof Object) {
+        return [...acc, { stat: '-', key, value: '', child: makeAstForCompare(inputObj1[key], inputObj1[key]) }];
+      }
+      return [...acc, { stat: '-', key, value: inputObj1[key], child: [] }];
     }
-    return acc.concat(`+ ${key}: ${inputObj2[key]}`);
+    if (inputObj2[key] instanceof Object) {
+      return [...acc, { stat: '+', key, value: '', child: makeAstForCompare(inputObj2[key], inputObj2[key]) }];
+    }
+    return [...acc, { stat: '+', key, value: inputObj2[key], child: [] }];
   }, []);
-  return `{\n\t${_.join(result, '\n\t')}\n}`;
+  return ast;
 };
 
-const readConfigFile = pathToFile => fs.readFileSync(pathToFile, 'utf8');
+const compareStr = (ast) => {
+  const result = _.reduce(ast, (acc, node) => {
+    const body = node.child.length === 0 ? node.value : compareStr(node.child);
+    return acc.concat(`\n  ${node.stat} ${node.key}: ${body}`);
+  }, '');
+  return `{${result}\n}`;
+};
+
+export const readConfigFile = pathToFile => fs.readFileSync(pathToFile, 'utf8');
 
 export default (pathToFile1, pathToFile2) => {
   const originStrData1 = readConfigFile(pathToFile1);
   const originStrData2 = readConfigFile(pathToFile2);
   const objForCompare1 = getParser(path.extname(pathToFile1))(originStrData1);
   const objForCompare2 = getParser(path.extname(pathToFile2))(originStrData2);
-  return compareStr(objForCompare1, objForCompare2);
-  // return objForCompare1;
+  // return compareStr(objForCompare1, objForCompare2);
+  return compareStr(makeAstForCompare(objForCompare1, objForCompare2));
 };
